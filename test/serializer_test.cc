1b0b63bb7d838df33a239ed19f23a966e11884f1
Mike Qin <mikeandmore@gmail.com>
2018-10-21 15:48:25 -0400

    add unittest for serializers

--- test/serializer_test.cc
+++ test/serializer_test.cc
@@ -1,20 +1,50 @@
 #include <gtest/gtest.h>
 
 #include "sqltypes.h"
+#include "benchmark/tpcc/tpcc.h"
 
 namespace felis {
 
 using sql::Tuple;
 
 class TupleTest : public testing::Test {
+ protected:
+  void *buf = nullptr;
+  virtual void SetUp() final override {
+    buf = malloc(4096);
+  }
+  virtual void TearDown() final override {
+    free(buf);
+  }
 };
 
 TEST_F(TupleTest, Simple) {
-  void *buf = alloca(4096);
   Tuple<int> t(10);
   VarStr *s = t.EncodeFromAlloca(buf);
-  t.Decode(s);
-  ASSERT_EQ(t._<0>(), 10);
+  Tuple<int> n;
+  n.Decode(s);
+  ASSERT_EQ(n._<0>(), 10);
+}
+
+TEST_F(TupleTest, Nested) {
+  Tuple<Tuple<int, int>> t(Tuple<int, int>(23, 24));
+  VarStr *s = t.EncodeFromAlloca(buf);
+  Tuple<Tuple<int, int>> n;
+  n.Decode(s);
+
+  ASSERT_EQ(n._<0>()._<0>(), 23);
+  ASSERT_EQ(n._<0>()._<1>(), 24);
+}
+
+TEST_F(TupleTest, NestedDBRow) {
+  Tuple<tpcc::Customer::Key> t(tpcc::Customer::Key::New(10, 1000, 12));
+  VarStr *s = t.EncodeFromAlloca(buf);
+  Tuple<tpcc::Customer::Key> n;
+  n.Decode(s);
+  const auto &k = n._<0>();
+  ASSERT_EQ(k.c_w_id, 10);
+  ASSERT_EQ(k.c_d_id, 1000);
+  ASSERT_EQ(k.c_id, 12);
 }
 
 }
