d9b7ce592b99a3506f84b636729aa1a745a54d5a
Mike Qin <mikeandmore@gmail.com>
2018-11-04 21:42:07 -0500

    OOps, we should not mix tabs and spaces

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,4 +1,5 @@
-((nil . ((projectile-project-compilation-cmd . "NAILGUN_SERVER=local:.buckd/sock ng com.facebook.buck.cli.Main build :db")))
+((nil . ((projectile-project-compilation-cmd . "NAILGUN_SERVER=local:.buckd/sock ng com.facebook.buck.cli.Main build :db")
+	 (indent-tabs-mode . nil)))
  (c-mode . ((mode . c++)
             (c-basic-offset . 2)))
  (c++-mode . ((c-basic-offset . 2)))
