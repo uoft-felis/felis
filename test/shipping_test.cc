3ce492814b4b2310ce2f5d42c5dbf0931874c942
Zhiqi He <hezhiqi734@gmail.com>
2018-11-09 13:06:13 -0500

    added MutableTest

--- test/shipping_test.cc
+++ test/shipping_test.cc
@@ -6,6 +6,7 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <thread>
+#include <random>
 
 namespace felis {
 
@@ -17,6 +18,8 @@ class TestObject {
  public:
   TestObject(int value) : value(value) {}
   TestObject() : value(0) {}
+
+  void SetValue(int value) { this->value = value; }
 
   ShippingHandle *shipping_handle() { return &handle; }
   void EncodeIOVec(struct iovec *vec) {
@@ -64,6 +67,62 @@ TEST_F(ShippingTest, SimplePipeTest) {
       break;
     }
   }
+  t.join();
+}
+
+void ThreadSend(TestObject obj[]) {
+  auto s = Shipment<TestObject>("127.0.0.1", 41346);
+  for (int i = 0; i < 2 << 17; i++) {
+    s.AddShipment(&obj[i]);
+  }
+  while (!s.RunSend());
+}
+
+ void ThreadUpdate(TestObject obj[]) {
+  auto s = Shipment<TestObject>("127.0.0.1", 41346);
+  std::random_device rd;
+  std::default_random_engine gen = std::default_random_engine(rd());
+  std::uniform_int_distribution<int> dis(1, 2 << 17);
+
+  for (int i = 0; i < 2 << 15; i++) {
+    int idx = dis(gen);
+    obj[idx].SetValue(i);
+    if (obj[idx].shipping_handle()->MarkDirty()) {
+      s.AddShipment(&obj[i]);
+    }
+  }
+ }
+
+TEST_F(ShippingTest, MutableTest) {
+  int srv = socket(AF_INET, SOCK_STREAM, 0);
+  int enable = 1;
+  setsockopt(srv, SOL_SOCKET, SO_REUSEADDR, &enable, sizeof(int));
+
+  go::TcpSocket::CommonInetAddr addr, claddr;
+  socklen_t socklen;
+  go::TcpSocket::FillSockAddr(addr, socklen, AF_INET, "127.0.0.1", 41346);
+  ASSERT_EQ(bind(srv, addr.sockaddr(), socklen), 0);
+  ASSERT_EQ(listen(srv, 1), 0);
+
+  TestObject obj[2<<17];
+  std::thread t(ThreadSend, obj);
+  std::thread t1(ThreadUpdate, obj);
+
+
+  int fd = accept(srv, addr.sockaddr(), &socklen);
+  ASSERT_GT(fd, 0);
+
+  auto s = ShipmentReceiver<TestObject>(fd);
+  
+  while (true)
+  {
+    TestObject o;
+    if (!s.Receive(&o))
+    {
+      break;
+    }
+  }
+  t1.join();
   t.join();
 }
 
