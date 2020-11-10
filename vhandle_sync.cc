0fed9ee624c2172d76ad0c528f62115e3896aebb
Zhiqi He <hezhiqi734@gmail.com>
2020-11-10 17:50:51 -0500

    correct logic between kDeletePendingValue and kPendingValue

--- vhandle_sync.cc
+++ vhandle_sync.cc
@@ -108,15 +108,22 @@ void SpinnerSlot::OfferData(volatile uintptr_t *addr, uintptr_t obj)
 
   // installing newval
   while (true) {
-    if (!IsPendingVal(oldval)) {
-      if (IsDeletePendingVal(oldval) && (!IsPendingVal(newval) || newval != (uintptr_t)nullptr))
-        continue; // spin
-      if (oldval == (uintptr_t)nullptr)
+    if (!IsPendingVal(oldval) && !IsDeletePendingVal(oldval)) {
+      if (oldval == (uintptr_t)nullptr) {
+        logger->critical("oldval is nullptr, addr {0:x}", (void*)addr);
         std::abort(); // batched txn's write is onto a deleted version, abort
-      if (IsPendingVal(newval))
+      }
+      if (IsDeletePendingVal(newval))
         return; // other core's write beats kDeletePendingVal's install
       logger->critical("strange oldval {0:x}", oldval);
       std::abort();
+    }
+    if (IsDeletePendingVal(oldval)) {
+      // if write to kDeletePendingValue is not kPending or nullptr, spin
+      if (!IsPendingVal(newval) && newval != (uintptr_t)nullptr) {
+        oldval = *addr;
+        continue;
+      }
     }
 
     uintptr_t val = __sync_val_compare_and_swap(addr, oldval, newval);
